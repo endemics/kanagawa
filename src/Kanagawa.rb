1a322c8368811fa42cfed207e8a8cf02a10f1d7f
Thomas Clavier <tom@tcweb.org>
2011-12-16 16:59:39 +0100

    Test now check return code

--- src/Kanagawa.rb
+++ src/Kanagawa.rb
@@ -1,9 +1,18 @@
+require "open4"
+
 class Kanagawa 
-	def initialize scenario
-		
+	def initialize(scenario, tsung_bin = "/usr/bin/tsung")
+		@scenario = scenario
+		@tsung_bin = tsung_bin
 	end
 
 	def run
-		return 1
+		status = Open4::popen4(@tsung_bin) do |stdout, stderr, stdin, pid|
+    end
+		if status.exitstatus.nil? then
+			return 1
+		else 
+			return status.exitstatus
+		end
 	end
 end
--- test/KanagawaTestCase.rb
+++ test/KanagawaTestCase.rb
@@ -2,11 +2,21 @@ require "../src/Kanagawa"
 require "test/unit"
 
 class KanagawaTestCase < Test::Unit::TestCase
-	def test_return_1_when_tsung_fails_to_run
-		#Given
+	def test_return_1_when_tsung_return_1
+		kanagawa = Kanagawa.new("tsung_test_fail.xml", "/bin/false")
+
+		assert_equal(1,kanagawa.run)
+	end
+
+	def test_return_1_when_tsung_returns_non_zero_code_or_fail_to_start
 		kanagawa = Kanagawa.new("tsung_test_fail.xml")
 
-		#Then	
 		assert_equal(1,kanagawa.run)
+	end
+
+	def test_return_0_when_tsung_returns_0
+		kanagawa = Kanagawa.new("tsung_test_minimal.xml","/bin/true")
+
+		assert_equal(0,kanagawa.run)
 	end
 end
