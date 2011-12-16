dd42f9a7bce8d2039aff49c1ba844ea7b69fd0e8
Thomas Clavier <tom@tcweb.org>
2011-12-16 18:46:14 +0100

    added mocks for tsung + real call to tsung; added simple reference scenario

--- src/Kanagawa.rb
+++ src/Kanagawa.rb
@@ -1,18 +1,36 @@
 require "open4"
 
 class Kanagawa 
-	def initialize(scenario, tsung_bin = "/usr/bin/tsung")
+	def initialize(scenario, tsung_bin = '/usr/bin/tsung')
 		@scenario = scenario
 		@tsung_bin = tsung_bin
 	end
 
 	def run
-		status = Open4::popen4(@tsung_bin) do |stdout, stderr, stdin, pid|
-    end
-		if status.exitstatus.nil? then
+		if ! File.exist?(@scenario)
 			return 1
-		else 
-			return status.exitstatus
 		end
+		exec="#{@tsung_bin} -f - -m - start"
+		status = Open4::popen4(exec) do |pid, stdin, stdout, stderr|
+			err_thread = Thread.new(stderr) do |stderr_lines|
+				while (line = stderr_lines.gets)
+	          puts "stderr: #{line}"
+				end
+			end
+
+			out_thread = Thread.new(stdout) do |stdout_lines|
+				while (line = stdout_lines.gets)
+				end
+			end
+
+			File.new(@scenario).each_line do |scenario_line|
+				stdin.puts(scenario_line)
+			end
+			stdin.close
+
+			err_thread.join
+			out_thread.join
+    end
+		status.exitstatus.nil? ? 1 : status.exitstatus
 	end
 end
--- test/KanagawaTestCase.rb
+++ test/KanagawaTestCase.rb
@@ -2,10 +2,8 @@ require "../src/Kanagawa"
 require "test/unit"
 
 class KanagawaTestCase < Test::Unit::TestCase
-	def test_return_1_when_tsung_return_1
-		kanagawa = Kanagawa.new("tsung_test_fail.xml", "/bin/false")
-
-		assert_equal(1,kanagawa.run)
+	def test_return_1_when_tsung_mock_return_1
+		assert_return_code_equal(1,"tsung_test_fail.xml","/bin/false")
 	end
 
 	def test_return_1_when_tsung_returns_non_zero_code_or_fail_to_start
@@ -14,9 +12,26 @@ class KanagawaTestCase < Test::Unit::TestCase
 		assert_equal(1,kanagawa.run)
 	end
 
-	def test_return_0_when_tsung_returns_0
-		kanagawa = Kanagawa.new("tsung_test_minimal.xml","/bin/true")
+	def test_return_0_when_tsung_mock_returns_0
+		assert_return_code_equal(0,"tsung_test_minimal.xml","../bin/return_true")
+	end
 
+	def test_return_1_when_scenario_does_not_exist
+		kanagawa = Kanagawa.new("tsung_test_not_exist.xml")
+
+		assert_equal(1,kanagawa.run)
+	end
+
+	def test_return_0_when_tsung_returns_0
+		kanagawa = Kanagawa.new("tsung_test_minimal.xml")
+		
 		assert_equal(0,kanagawa.run)
 	end
 end
+
+def assert_return_code_equal return_code, scenario, bin
+		kanagawa = Kanagawa.new(scenario,bin)
+
+		assert_equal(return_code,kanagawa.run)
+end
+
