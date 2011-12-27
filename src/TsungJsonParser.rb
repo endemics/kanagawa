168deeae9a0e8347ca6313bb768246f77dfb0eb5
Thomas Clavier <tom@tcweb.org>
2011-12-27 14:52:44 +0100

    adding brut json from tsung output

--- src/TsungJsonParser.rb
+++ src/TsungJsonParser.rb
@@ -5,23 +5,34 @@ class TsungJsonParser
 		@datas = Array.new
 	end
 
+	# to add one line directly output from JSON tsung this function will cleanup the string before call of add_json
+	def add_string(input)
+		return 0 if input.nil?
+
+		if input.include? "timestamp"
+			# remove last ","
+			input = input.sub(/,\s*$/, " ")
+			# remove last "]}" they correspond to close of '{"stats": ['
+			input = input.sub(/\}\]\}\]\}\s*$/, "}]}") unless input.nil?
+
+			add_json(input)
+		end
+	end
+
 	# to add one data burst in json format
 	# one timestamp with samples : {"timestamp": XXXX,  "samples": [ .... ]} 
 	def add_json(string)
-		if ! string.nil?
-			tmp=JSON.parse(string) 
-			@datas << tmp
-		end
+		@datas << JSON.parse(string) unless string.nil?
 	end
 
 	def status?
 		return_code = :ok
 
-		@datas.last['samples'].each { |sample|
+		@datas.last['samples'].each do |sample|
 			if sample['name'] == 'error_connect_nxdomain' && sample['total'] >= 1
 				return_code = :break
 			end
-		}
+		end
 
 		return return_code
 	end
--- test/TsungJsonParserTestCase.rb
+++ test/TsungJsonParserTestCase.rb
@@ -37,4 +37,35 @@ class TsungJsonParserTestCase < Test::Unit::TestCase
 		tsung_parser.add_json(nil)
 		assert_equal(0,tsung_parser.count)
 	end
+
+	def test_i_can_add_data_take_from_tsung_json_output
+		tsung_parser = TsungJsonParser.new()
+		json_data = '"Log directory is: /home/tom/.tsung/log/20111227-0007"'
+		tsung_parser.add_string(json_data)
+		assert_equal(0,tsung_parser.count, "Insert of 'Log directory' information)")
+
+		json_data = '{ '
+		tsung_parser.add_string(json_data)
+		assert_equal(0,tsung_parser.count, "Insert of first {")
+
+		json_data = '	 "stats": ['
+		tsung_parser.add_string(json_data)
+		assert_equal(0,tsung_parser.count, "Insert of stats struct")
+
+		json_data = '		  {"timestamp": 1324940864,  "samples": []},'
+		tsung_parser.add_string(json_data)
+		assert_equal(1,tsung_parser.count, "Insert of first good line")
+
+		json_data = '		  {"timestamp": 1324940874,  "samples": [   {"name": "users", "value": 0, "max": 0}, {"name": "users_count", "value": 0, "total": 0}, {"name": "finish_users_count", "value": 0, "total": 0}]},'
+		tsung_parser.add_string(json_data)
+		assert_equal(2,tsung_parser.count, "Insert of second good line")
+
+		json_data = '			 {"timestamp": 1324940879,  "samples": [   {"name": "users", "value": 0, "max": 1}, {"name": "freemem", "hostname": "localhost", "value": 1, "mean": 2932.63671875,"stdvar": 0,"max": 2932.63671875,"min": 2932.63671875 ,"global_mean": 0 ,"global_count": 0}, {"name": "cpu", "hostname": "localhost", "value": 1, "mean": 2.246069378587472,"stdvar": 0,"max": 2.246069378587472,"min": 2.246069378587472 ,"global_mean": 0 ,"global_count": 0}, {"name": "load", "hostname": "localhost", "value": 1, "mean": 0.1015625,"stdvar": 0,"max": 0.1015625,"min": 0.1015625 ,"global_mean": 0 ,"global_count": 0}, {"name": "session", "value": 1, "mean": 228.4990234375,"stdvar": 0,"max":  228.4990234375,"min": 228.4990234375 ,"global_mean": 0 ,"global_count": 0}, {"name": "users_count", "value": 1, "total": 1}, {"name": "finish_users_count", "value": 1, "total": 1}, {"name": "request", "value": 1, "mean": 185.360107421875,"stdvar": 0.0,"max":  185.360107421875,"min": 185.360107421875 ,"global_mean": 0 ,"global_count": 0}, {"name": "page", "value": 1, "mean": 185.360107421875,"stdvar": 0.0,"max":  185.360107421875,"min": 185.360107421875 ,"global_mean": 0 ,"global_count": 0}, {"name": "connect", "value": 1, "mean": 108.09619140625,"stdvar": 0.0,"max":  108.09619140625,"min": 108.09619140625 ,"global_mean": 0 ,"global_count": 0}, {"name": "tr_01_root", "value": 1, "mean": 227.366943359375,"stdvar": 0,"max":  227.366943359375,"min": 227.366943359375 ,"global_mean": 0 ,"global_count": 0}, {"name": "match", "value": 1, "total": 1}, {"name": "http_200", "value": 1, "total": 1}, {"name": "size_rcv", "value": 3666, "total": 3666}, {"name": "size_sent", "value": 54, "total": 54}, {"name": "connected", "value": 0, "max": 0}, {"name": "error_abort", "value": 1, "total": 1}]}]}'
+		tsung_parser.add_string(json_data)
+		assert_equal(3,tsung_parser.count, "Insert of last good line")
+
+		json_data = ' '
+		tsung_parser.add_string(json_data)
+		assert_equal(3,tsung_parser.count, "Add empty line")
+	end
 end
